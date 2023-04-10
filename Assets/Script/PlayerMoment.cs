664c42c0397a3bd3da0d0b27a8a36cfc5fbd79ab
Fredu95 <fredue18@gmail.com>
2023-04-10 14:42:00 +0300

    playermoment

--- Assets/Script/PlayerMoment.cs
+++ Assets/Script/PlayerMoment.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMoment : MonoBehaviour
 {   
     Rigidbody2D rb;
+    [SerializeField]
+    float thrust = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +16,21 @@ public class PlayerMoment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-if (Input.GetKeyDown(KeyCode.Space))
-    {
-        rb.AddForce(new Vector2(0,30));
-    }
-if (Input.GetKeyDown(KeyCode.A))
-    {
+if (Input.GetButton("Jump"))
+        {
+        rb.AddForce(transform.up * thrust * Time.deltaTime);//Liikkuu eteenpain
+        }
     
-    }
+    float xInput= Input.GetAxis("Horizontal");
+    float speed = 50;
+
+    gameObject.transform.Rotate(new Vector3(0f, 0f, xInput * Time.deltaTime * speed)); //Kaantyy paikallaan, keyboard väärinpäin?
 
     }
+
+void OnCollisionEnter2D(Collision2D other)
+{
+ Debug.Log("coli");   
+}
+
 }
